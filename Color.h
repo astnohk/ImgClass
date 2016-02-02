6caab049f49354f34dbb084ee243012e27a24b86
sh-konta <kon.bass@gmail.com>
2016-02-02 20:26:55 +0900

    Add Color header and source

---
+++ Color.h
@@ -0,0 +1,12 @@
+#ifndef LIB_ImgClass_COLOR_H
+#define LIB_ImgClass_COLOR_H
+
+#include <cmath>
+
+double inner_prod(const double& lvalue, const double& rvalue);
+
+double norm(const double& value);
+double norm_squared(const double& value);
+
+#endif
+
